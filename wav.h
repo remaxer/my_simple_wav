ed115ffa64feb7955707668ee723090d646f226f
REmaxer <remaxer@hotmail.it>
2013-01-29 16:12:54 +0100

    first commit

--- wav.h
+++ wav.h
@@ -3,46 +3,45 @@
  *  WAV FILE STRUCTURE
  *
  *  @Author:REmaxer	
+ *  @Version:1.0
+ *
  */
 
-//define byte type
+
+/* define byte type */
 typedef unsigned char byte;
 
+/*
+ *	General RIFF header
+ */
 struct riff_header_chunk{
-	// 
-	unsigned int chunk_id;
-	//
-	unsigned int chunk_size;	//BigE
-	//
-	unsigned int format;
+	unsigned int chunk_id;		/* "RIFF" in ASCII (big-endian)*/
+	unsigned int chunk_size;	/* Size of entire file minus chunk_id and chunk_size */
+	unsigned int format;		/* File format (Wav Files = "WAVE" in ASCII ) (big-endian)*/
 };	
 
+/*
+ *	Fmt header
+ */
 struct fmt_chunk{
-	//
-	unsigned int schunk1_id;
-	//
-	unsigned int schunk1_size;	//BigE
-	//
-	unsigned short audio_format;	//BigE
-	//	
-	unsigned short num_channels;	//BigE
-	//
-	unsigned int sample_rate;
-	//
-	unsigned int byte_rate;
-	//
-	unsigned short block_align;
-	//
-	unsigned short  bits_per_sample;
+	unsigned int schunk1_id;	/* "fmt " in ASCII (big-endian)*/
+	unsigned int schunk1_size;	/* Size of the rest of subchunk fmt (16 for PCM ) */
+	unsigned short audio_format;	/* Audio format ( 1 for PCM) */
+	unsigned short num_channels;	/* Number of channels */
+	unsigned int sample_rate;	/* Samples per seconds */
+	unsigned int byte_rate;		/* Bytes per seconds ( sample_rate * block_align )*/
+	unsigned short block_align;	/* Bytes per sample  ( including all channels ) */	
+	unsigned short bits_per_sample; /* Bits per sample (excluding channels ) */
 };
 
+/*
+ *	Data header
+ */
+
 struct data_chunk{
-	//
-	unsigned int schunk2_id;
-	//
-	unsigned int schunk2_size;
-	//data pointer
-	byte *data;
+	unsigned int schunk2_id;	/* "DATA" in ASCII (big-endian)*/
+	unsigned int schunk2_size;	/* Bytes in data ( num_samples * block_align )*/
+	byte *data;			/* Generic data pointer */
 };
 	
 
